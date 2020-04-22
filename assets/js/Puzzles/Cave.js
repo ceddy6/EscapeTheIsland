c86e1a31635201ba6ba7543f4c8024dcb9923d3b
ceddy6 <63300845+ceddy6@users.noreply.github.com>
2020-04-22 09:47:52 +0100

    Make stones properly draggable

--- assets/js/Puzzles/Cave.js
+++ assets/js/Puzzles/Cave.js
@@ -19,42 +19,37 @@ class CavePuzzle{
                     })
                 )
 
+        // Append some spaces for the stones 
+        var hollow1 = $('<div class="click-region hollow" id="hollow1"></div>')
+        var hollow2 = $('<div class="click-region hollow" id="hollow2"></div>')
+        var hollow3 = $('<div class="click-region hollow" id="hollow3"></div>')
+        var hollow4 = $('<div class="click-region hollow" id="hollow4"></div>')
+        canvas.append(hollow1).append(hollow2).append(hollow3).append(hollow4)
+
         // Append some stones 
         var stone1 = $('<div class="click-region stone" id="stone1"></div>')
         var stone2 = $('<div class="click-region stone" id="stone2"></div>')
         var stone3 = $('<div class="click-region stone" id="stone3"></div>')
         var stone4 = $('<div class="click-region stone" id="stone4"></div>')
         canvas.append(stone1).append(stone2).append(stone3).append(stone4)
+      
+        // Add jquery method to make the stones draggable
+        $('.stone').draggable({
+            // Keep them in the right window
+            containment:$('#minigame-modal').find('.modal-body'),
+            scroll:true,
+            snap:'.hollow',
+            snapMode:"inner",
+            snapTolerance:"20"
+        })
 
-        // Make the stones elements draggable:
-        dragElement($("#stone1")[0])
-        dragElement($("#stone2")[0])
-        dragElement($("#stone3")[0])
-        dragElement($("#stone4")[0])
-
-        // Add on click to the stones to make them zoomable
+        //Add on click to the stones to make them zoomable
         $('.stone').on("click",function(){
             // Fill the zoom modal with the image and open it
             var zoom = $('#zoom-modal')
             zoom.find('.modal-title').text("There are markings on the stones") 
-            switch(this.id) {
-                case "stone1": 
-                    zoom.find('.modal-body').empty()
-                    .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/stone1.bmp alt="Cave Stones">')
-                    break;
-                case "stone2":
-                    zoom.find('.modal-body').empty()
-                    .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/stone2.bmp alt="Cave Stones">')
-                    break;
-                case "stone3":
-                    zoom.find('.modal-body').empty()
-                    .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/stone3.bmp alt="Cave Stones">')
-                    break;
-                case "stone4":
-                    zoom.find('.modal-body').empty()
-                    .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/stone4.bmp alt="Cave Stones">')
-                    break;
-            }
+            zoom.find('.modal-body').empty()
+                .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/'+this.id+'.bmp alt="Cave Stones">')
             zoom.modal('show')
         })
 
