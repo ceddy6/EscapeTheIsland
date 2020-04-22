bc7da9431ae773bfd2d696a31f5ba4d69f402266
ceddy6 <63300845+ceddy6@users.noreply.github.com>
2020-04-22 08:02:15 +0100

    Add stones to cave

--- assets/js/Puzzles/Cave.js
+++ assets/js/Puzzles/Cave.js
@@ -32,6 +32,32 @@ class CavePuzzle{
         dragElement($("#stone3")[0])
         dragElement($("#stone4")[0])
 
+        // Add on click to the stones to make them zoomable
+        $('.stone').on("click",function(){
+            // Fill the zoom modal with the image and open it
+            var zoom = $('#zoom-modal')
+            zoom.find('.modal-title').text("There are markings on the stones") 
+            switch(this.id) {
+                case "stone1": 
+                    zoom.find('.modal-body').empty()
+                    .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/stone1.bmp alt="Cave Stones">')
+                    break;
+                case "stone2":
+                    zoom.find('.modal-body').empty()
+                    .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/stone2.bmp alt="Cave Stones">')
+                    break;
+                case "stone3":
+                    zoom.find('.modal-body').empty()
+                    .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/stone3.bmp alt="Cave Stones">')
+                    break;
+                case "stone4":
+                    zoom.find('.modal-body').empty()
+                    .append('<img class="img-fluid" id="zoomed-cave-stone" src=assets/images/minigames/puzzles/cave/stone4.bmp alt="Cave Stones">')
+                    break;
+            }
+            zoom.modal('show')
+        })
+
     }
 
 }
