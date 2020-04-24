0a69044c0dba109a8d0c0bf5fa08723c787380c0
ceddy6 <63300845+ceddy6@users.noreply.github.com>
2020-04-24 09:52:18 +0100

    Add view or use when clicking on an inventory item

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -1,8 +1,8 @@
 // Outside the inventory class, keep a list of the possible items that could go into the inventory
 var inventoryItems = [
-    {title:"padlock-key",path:"assets/images/locks/padlock-key.jpg"},
-    {title:"stepping-stones-clue",path:"assets/images/minigames/puzzles/cave/clue.bmp"},
-    {title:"waterfall-clue",path:"assets/images/minigames/puzzles/cave/clue2.bmp"}
+    {title:"padlock-key",path:"assets/images/locks/padlock-key.jpg",description:"An old key",useability:"inventory-item-useable"},
+    {title:"stepping-stones-clue",path:"assets/images/minigames/puzzles/cave/clue.bmp",description:"Some kind of diagram",useability:"inventory-item-viewable"},
+    {title:"waterfall-clue",path:"assets/images/minigames/puzzles/cave/clue2.bmp",description:"Sounds like a riddle",useability:"inventory-item-viewable"}
 ]
 
 // Create a class for the inventory
@@ -27,7 +27,7 @@ class Inventory {
 
         // Add the image to the modal 
         $('#inventory-modal').find('.modal-body')
-            .append('<img class="img-fluid inventory-item" id='+item+'-inventory src='+details.path+' alt='+item+'>')
+            .append('<img class="img-fluid inventory-item '+details.useability+'" id='+item+'-inventory src='+details.path+' alt='+item+'>')
         
             // Include an on-click method for that specific item
             .on("click",function(){inventory.itemClicked(item)})
@@ -53,26 +53,40 @@ class Inventory {
     // Inventory has a method to handle items in the inventory being clicked on
     itemClicked(item) {
 
-        // Check which item was clicked on
-        switch(item) {
-  
-            // If it's the key to the first padlock
-            case 'padlock-key':
-                console.log("Key clicked on")
-                // Need to check whether the lock is the right one for the key
-                if (lock.index == 0) {
-                    $('#lock-modal').find('.modal-body')
-                        .empty()
-                        .append('<img class="img-fluid" id="lock-background" src='+lockData[lock.index].inter_img+' alt="Lock with key">')
-                        // Add a div to flash red or green on success or failure
-                        .append($('<div id="outlineflash"></div>'))
-                }
-                lock.addToLockPath("none")
-                break;
-        }
-
         // Close the inventory modal
         $('#inventory-modal').modal('hide')
+
+        // Get the entry of the image from the inventory list
+        var details = inventoryItems.find(function(entry){return entry.title == item})
+
+        // If the inventory item is useable, use it, if not, open a zoom
+        if (details.useability == "inventory-item-useable") {
+
+            // Check which item was clicked on
+            switch(item) {
+    
+                // If it's the key to the first padlock
+                case 'padlock-key':
+                    console.log("Key clicked on")
+                    // Need to check whether the lock is the right one for the key
+                    if (lock.index == 0) {
+                        $('#lock-modal').find('.modal-body')
+                            .empty()
+                            .append('<img class="img-fluid" id="lock-background" src='+lockData[lock.index].inter_img+' alt="Lock with key">')
+                            // Add a div to flash red or green on success or failure
+                            .append($('<div id="outlineflash"></div>'))
+                    }
+                    lock.addToLockPath("none")
+                    break;
+            }
+
+        // The item is a 'viewable' item, so open a zoom
+        } else {
+
+            // Open up a zoom modal
+            zoom = new Zoom(details.path,details.title,details.description,0)
+
+        }
 
     }
 
