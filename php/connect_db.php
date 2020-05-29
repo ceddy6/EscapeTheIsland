0c4f1625a2e8f90269a131de0fccaf23f1123b3d
ceddy6 <63300845+ceddy6@users.noreply.github.com>
2020-05-29 15:47:56 +0100

    Re-add database connection

--- php/connect_db.php
+++ php/connect_db.php
@@ -1,14 +1,18 @@
 
 <?php
 
-    $dsn = "pgsql:"
-        . "host=ec2-54-75-246-118.eu-west-1.compute.amazonaws.com;"
-        . "dbname=d7fgg9jaca0eio;"
-        . "user=xsefhvcvgjlgtx;"
-        . "port=5432;"
-        . "sslmode=require;"
-        . "password=fed355770cb4fecb884ea1ce5cfcded113339d2403d5e13088fc950bd18e4e63";
-
-    $db = new PDO($dsn);
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+    $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+                array(
+                    'pdo.server' => array(
+                    'driver'   => 'pgsql',
+                    'user' => $dbopts["user"],
+                    'password' => $dbopts["pass"],
+                    'host' => $dbopts["host"],
+                    'port' => $dbopts["port"],
+                    'dbname' => ltrim($dbopts["path"],'/')
+                    )
+                )
+    );
 
 ?>
