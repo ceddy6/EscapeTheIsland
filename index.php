ef6bb6ee8f3fa0282e1da5385589695e374c438e
ceddy6 <63300845+ceddy6@users.noreply.github.com>
2020-05-29 11:47:48 +0100

    Tidying index files

--- index.php
+++ index.php
@@ -63,7 +63,6 @@
     <!-- Elements for the background map and clock -->
     <div class="container-fluid" id="background-map-wrapper">
         <img class="img-fluid" id="background-map" src="assets/images/map3.jpg" alt="Map">    
-        <?php include_once("php/getLeaderboard.php"); ?>
         <div class="background-clock-wrapper container-fluid">
           <img class="img-fluid" id="background-clock" src="assets/images/clock2.jpg" alt="Clock">
           <div class="background-clock-digits-wrapper container-fluid">
