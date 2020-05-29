004c34d5114f2b8944245e3e6744d1575023c4d9
ceddy6 <63300845+ceddy6@users.noreply.github.com>
2020-05-29 11:40:54 +0100

    Move html file into php file

---
+++ php/getLeaderboard.php
@@ -0,0 +1,8 @@
+<?php
+    $query = "SELECT * FROM leaderboard ORDER BY total_time ASC;";
+    $result = $db->query($query);
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo "<span>" . $row["name"] . "</span>";
+    }
+    $result->closeCursor();
+?>
