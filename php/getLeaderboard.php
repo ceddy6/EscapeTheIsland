272950a3a16ab162a0e934063e7ef0c364af13d9
ceddy6 <63300845+ceddy6@users.noreply.github.com>
2020-05-29 15:57:04 +0100

    Use test message instead of db query

--- php/getLeaderboard.php
+++ php/getLeaderboard.php
@@ -1,8 +1,9 @@
 <?php
-    $query = "SELECT * FROM leaderboard ORDER BY total_time ASC;";
-    $result = $db->query($query);
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        echo "<span>" . $row["name"] . "</span>";
-    }
-    $result->closeCursor();
+    echo "<span>THIS IS A TEST</SPAN>";
+    // $query = "SELECT * FROM leaderboard ORDER BY total_time ASC;";
+    // $result = $db->query($query);
+    // while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    //     echo "<span>" . $row["name"] . "</span>";
+    // }
+    // $result->closeCursor();
 ?>
