2a05eaa3050b5b49402f732335b1012707c81329
ceddy6 <63300845+ceddy6@users.noreply.github.com>
2020-05-30 08:37:44 +0100

    Change syntax of db connect

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- Connect to the database -->
-<?php include_once("../php/connect_db.php"); ?>
+<?php require "./php/connect_db.php"; ?>
 
 <!-- Run the game -->
 <?php //include_once("index.html"); ?>
